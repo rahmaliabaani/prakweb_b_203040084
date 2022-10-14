0f841cdd6bae39005abf8e50733a95de7cee987c
rahmaliabaani <203040084@mail.unpas.ac.id>
2022-10-14 14:53:05 +0700

    Video#7 Model

--- phpmvc/app/views/templates/header.php
+++ phpmvc/app/views/templates/header.php
@@ -11,7 +11,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand" href="#">PHP MVC</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -19,6 +19,7 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link active" aria-current="page" href="<?php echo BASEURL; ?>">Home</a>
+                    <a class="nav-link" href="<?php echo BASEURL; ?>/mahasiswa">Mahasiswa</a>
                     <a class="nav-link" href="<?php echo BASEURL; ?>/about">About</a>
                 </div>
             </div>
